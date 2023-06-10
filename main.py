109ca105c3b48b5ba83c6d099a19f028cda588c5
Nixuge <mail@nixuge.me>
2023-06-10 19:46:50 +0200

    Lowered down to 10, again just in case

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def run_batch_raw(servers: list[JavaServerSv | BedrockServerSv]):
     print("== Done with batch ==")
 
 
-async def run_batch_limit(servers: list[JavaServerSv | BedrockServerSv], task_limit: int = 15):
+async def run_batch_limit(servers: list[JavaServerSv | BedrockServerSv], task_limit: int = 10):
     print("== Starting batch ==")
 
     running_tasks: list[Task] = []
