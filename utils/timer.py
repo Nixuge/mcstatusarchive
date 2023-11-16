19a5d1bf24a7fd6742e3a5fd9eaf51996af88846
Nixuge <mail@nixuge.me>
2023-11-16 22:41:26 +0100

    Simplified Timer

--- utils/timer.py
+++ utils/timer.py
@@ -3,24 +3,17 @@ import time
 
 class Timer:
     start_time: int
-    last_step_time: int
     def __init__(self) -> None:
         self.start_time = time.time_ns()
-        self.last_step_time = 0
     
     # calculate timing
-    def _ct(self, minus: int) -> str:
-        return "{:.2f}".format((time.time_ns() - minus) / 1000000000)
+    def _ct(self) -> str:
+        return "{:.2f}".format((time.time_ns() - self.start_time) / 1000000000)
 
     def step(self) -> str:
-        if self.last_step_time != 0:
-            to_return = f"Took {self._ct(self.last_step_time)}s since last step, {self._ct(self.start_time)}s since the beginning"
-            self.last_step_time = time.time_ns()
-            return to_return
-        return f"Took {self._ct(self.start_time)}s"
-        
+        to_return = f"Took {self._ct()}s"
+        self.start_time = time.time_ns()
+        return to_return
 
     def end(self) -> str:
-        if self.last_step_time != 0:
-            return f"Took {self._ct(self.last_step_time)}s since last step, {self._ct(self.start_time)}s total"
-        return f"Took {self._ct(self.start_time)}s"
+        return f"Took {self._ct()}s"
