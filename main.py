3320bee11c2d9d503e7ccdce1d9fed703d72fc88
Nixuge <mail@nixuge.me>
2023-12-02 20:52:41 +0100

    Implemented INVALID_JAVA_SERVERS in main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Needs to be on top of everything for it to propagate to imports
 import logging
 from utils.logger import get_proper_logger
+from vars.InvalidServers import INVALID_JAVA_SERVERS, InvalidServers
 DEBUG_LOG = False
 logger = get_proper_logger(logging.getLogger("root"), DEBUG_LOG)
 
@@ -34,13 +35,20 @@ from vars.config import Timings
 from vars.DbQueues import DBQUEUES
 
 async def save_every_x_secs(servers: list):
+    i = 1
     while True:
+        try_invalid = False
+        if i >= InvalidServers.RETRY_INVALID_EVERY:
+            try_invalid = True
+            i = 0
+        i += 1
+
         if ErrorHandler.should_stop:
             logging.critical("KILLING THE APP.")
             os.kill(os.getpid(), signal.SIGUSR1)
         
         start_time = int(time())
-        await run_batch_limit(servers)
+        await run_batch_limit(servers, try_invalid)
 
         logging.info("[Waiting for timer to finish...]")
         while start_time + Timings.SAVE_EVERY > int(time()):
@@ -57,13 +65,25 @@ async def run_batch_raw(servers: list[JavaServerSv | BedrockServerSv]):
     logging.info("== Done with batch ==")
 
 
-async def run_batch_limit(servers: list[JavaServerSv | BedrockServerSv], task_limit: int = 75):
+async def run_batch_limit(servers: list[JavaServerSv | BedrockServerSv], try_invalid: bool = False, task_limit: int = 75):
     logging.info("== Starting batch ==")
     timer = Timer()
 
     running_tasks: list[Task] = []
 
-    to_add = list(servers)
+    to_add = []
+    for server in servers:
+        if type(server) == JavaServerSv:
+            if not INVALID_JAVA_SERVERS.is_invalid(server.ip):
+                to_add.append(server)
+            elif try_invalid: 
+                to_add.append(server)
+            # else:
+                # print(f"Skipped server: {server.ip}")
+        elif type(server) == BedrockServerSv:
+            to_add.append(server)
+    print(len(to_add))
+    # to_add = list(servers)
 
     while True:
         for server in to_add:
